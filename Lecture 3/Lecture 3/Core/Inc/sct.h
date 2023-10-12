48c88e9b4827e183bca46fc5e023e370f4bf4895
Jiri Veverka <jiri.veverka98@gmail.com>
2023-10-12 15:44:37 +0200

    Lecture 3: Add project

---
+++ Lecture 3/Lecture 3/Core/Inc/sct.h
@@ -0,0 +1,19 @@
+/*
+ * sct.h
+ *
+ *  Created on: Oct 12, 2023
+ *      Author: xvever12
+ */
+
+#ifndef INC_SCT_H_
+#define INC_SCT_H_
+
+#include "main.h"
+
+void sct_led(uint32_t value);
+
+void sct_value(uint16_t value);
+
+void sct_init(void);
+
+#endif /* INC_SCT_H_ */
